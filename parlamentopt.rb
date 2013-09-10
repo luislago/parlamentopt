3ade45bd6ef9d6931a771bb2cd039b063d3d873e
Luís Lago <luisalveslago@gmail.com>
2013-09-10 17:45:59 +0100

    automatic file name

--- parlamentopt.rb
+++ parlamentopt.rb
@@ -5,8 +5,8 @@ require "open-uri"
 puts "URL: "
 remote_full_url = gets.chomp #link to be parsed
 remote_data = Nokogiri::HTML(open(remote_full_url))   
-local_file_name = gets.chomp # I want to automate this step, it will be something like the next line, I think:
-#local_file_name = (remote_data.css("td#ctl00_ctl13_g_8035397e_bdf3_4dc3_b9fb_8732bb699c12_ctl00_ucNome_rptContent_ctl01_lblText").text)
+#local_file_name = gets.chomp # I want to automate this step, it will be something like the next line, I think:
+local_file_name = (remote_data.css("span#ctl00_ctl13_g_8035397e_bdf3_4dc3_b9fb_8732bb699c12_ctl00_ucNome_rptContent_ctl01_lblText").text)
 my_local_filename = local_file_name + ".txt" # 
 my_local_file = open(my_local_filename, "w") # creates the .txt file where the text will be copied
 my_local_file.write(remote_full_url) #copies the URL to the file
