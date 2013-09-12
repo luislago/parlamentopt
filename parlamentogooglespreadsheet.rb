d1f6b27470bbfca13735a5fbf92d7a42e8440adf
Luís Lago <luisalveslago@gmail.com>
2013-09-12 14:14:47 +0100

    some comments

--- parlamentogooglespreadsheet.rb
+++ parlamentogooglespreadsheet.rb
@@ -22,28 +22,28 @@ remote_data = Nokogiri::HTML(open(remote_full_url))
 # I also need to figure out how to handle things like "Actividades" 
 #(span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_gvActividades_ctl02_lblActividade), where some of the 
 #Members have more than one entry. the <span> is always identical, with a different number before "_lblText"
-ws[2,1] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblNomeCompleto").text)
-ws[2,2] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblActividadePrincipal").text)
-ws[2,3] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblEstadoCivil").text)
-ws[2,4] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblNomeConjuge").text)
-ws[2,5] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblRegimeBens").text)
-ws[2,6] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_gvActividades_ctl02_lblActividade").text)
-ws[2,7] = (remote_data.css("").text)
-ws[2,8] = (remote_data.css("").text)
-ws[2,9] = (remote_data.css("").text)
-ws[2,10] = (remote_data.css("").text)
-ws[2,11] = (remote_data.css("").text)
-ws[2,12] = (remote_data.css("").text)
-ws[2,13] = (remote_data.css("").text)
-ws[2,14] = (remote_data.css("").text)
-ws[2,15] = (remote_data.css("").text)
-ws[2,16] = (remote_data.css("").text)
-ws[2,17] = (remote_data.css("").text)
-ws[2,18] = (remote_data.css("").text)
-ws[2,19] = (remote_data.css("").text)
-ws[2,20] = (remote_data.css("").text)
-ws[2,21] = (remote_data.css("").text)
-ws[2,22] = (remote_data.css("").text)
+ws[2,1] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblNomeCompleto").text) #Nome Completo	
+ws[2,2] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblActividadePrincipal").text)#Actividade Principal
+ws[2,3] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblEstadoCivil").text) #Estado Civil
+ws[2,4] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblNomeConjuge").text)#Conjuge
+ws[2,5] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_lblRegimeBens").text)#Regime de Bens
+ws[2,6] = (remote_data.css("span#ctl00_ctl13_g_e4f66c46_a162_4329_83cb_3d7fe77e4b57_ctl00_gvActividades_ctl02_lblActividade").text)#Actividade
+ws[2,7] = (remote_data.css("").text)#Actividade Inicio
+ws[2,8] = (remote_data.css("").text)#Actividade Fim
+ws[2,9] = (remote_data.css("").text)#Remunerada
+ws[2,10] = (remote_data.css("").text)#Cargo
+ws[2,11] = (remote_data.css("").text)#Entidade
+ws[2,12] = (remote_data.css("").text)#Cargo Inicio
+ws[2,13] = (remote_data.css("").text)#Cargo Fim
+ws[2,14] = (remote_data.css("").text)#Natureza
+ws[2,15] = (remote_data.css("").text)#Sede
+ws[2,16] = (remote_data.css("").text)#ApoiosBeneficios
+ws[2,17] = (remote_data.css("").text)#Servicos Prestados
+ws[2,18] = (remote_data.css("").text)#Entidade Sociedades
+ws[2,19] = (remote_data.css("").text)#Area de Actividade
+ws[2,20] = (remote_data.css("").text)#Local da sede
+ws[2,21] = (remote_data.css("").text)#Participacao Social
+ws[2,22] = (remote_data.css("").text)#Outras situacoes	
 
 #  
 ws.save()
